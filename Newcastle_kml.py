17eeeb9428f40cb85ee2419c7764e61787503fd8
Michael Hemming <m.hemming@unsw.edu.au>
2020-07-29 15:40:02 +1000

    Moderate changes
    o changed file location
    o Added more map assets from csv (CH radar, proposed Sydney radar, other ATF moorings)
    o changed color scheme (green = existing, red = proposed)
    o changed glider color

--- Newcastle_kml.py
+++ Newcastle_kml.py
@@ -11,7 +11,7 @@ import simplekml
 
 # %% ------------------------------------------------
 # create Newcastle KML
-inputfile = csv.reader(open('/Users/Michael/Documents/Projects/Python_Google_map/Newcastle_locations.csv','r'))
+inputfile = csv.reader(open('/Users/Michael/Documents/Projects/Python_Google_map/IMOS_assets_map_creator/Newcastle_locations.csv','r'))
 
 kml=simplekml.Kml()
 
@@ -19,24 +19,75 @@ n = 0
 for row in inputfile:
     n = n+1  
     if n< 6:
-        pnt = kml.newpoint(name=row[0], coords=[(row[2],row[1])])        
-        pnt.style.iconstyle.color = simplekml.Color.white
+        # ATF moorings
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])        
+        pnt.style.iconstyle.color = simplekml.Color.green
         pnt.style.labelstyle.scale = 1 
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/donut.png'
     if n >= 6 and n < 8:
-        pnt = kml.newpoint(name=row[0], coords=[(row[2],row[1])])              
+        # HF radar sites
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
         pnt.style.iconstyle.color = simplekml.Color.green
         pnt.style.labelstyle.scale = 1 
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'  
     if n >= 8 and n < 16:
         a = 1
-    if n >= 16:
-        pnt = kml.newpoint(name=row[0], coords=[(row[2],row[1])])              
+        # HF radar field
+        #        pnt = kml.newpoint(name=row[0], coords=[(row[2],row[1])])              
+        #        pnt.style.iconstyle.color = simplekml.Color.black
+        #        pnt.style.labelstyle.scale = 1 
+        #        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'  
+    if n >= 16 and n < 19:
+        # Proposed Newcastle moorings
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
         pnt.style.labelstyle.scale = 2 
         pnt.style.iconstyle.color = simplekml.Color.red
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_square.png'         
-        
-
+    if n >= 19 and n < 21: 
+        # Proposed Sydney HF Radar sites
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
+        pnt.style.iconstyle.color = simplekml.Color.red
+        pnt.style.labelstyle.scale = 1 
+        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'  
+    if n >= 21 and n < 30:
+        a = 1
+        # Proposed Sydney HF radar field
+        #        pnt = kml.newpoint(name=row[0], coords=[(row[2],row[1])])              
+        #        pnt.style.iconstyle.color = simplekml.Color.black
+        #        pnt.style.labelstyle.scale = 1 
+        #        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'  
+    if n >= 30 and n < 32:        
+        # Coffs Harbour radar sites
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
+        pnt.style.iconstyle.color = simplekml.Color.green
+        pnt.style.labelstyle.scale = 1 
+        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'          
+    if n >= 32 and n < 39:
+        a = 1
+        # Coffs Harbour HF radar field
+        #        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
+        #        pnt.style.iconstyle.color = simplekml.Color.black
+        #        pnt.style.labelstyle.scale = 1 
+        #        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'          
+    if n >= 40 and n < 49:
+        # Other NSW Moorings
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
+        pnt.style.labelstyle.scale = 2 
+        pnt.style.iconstyle.color = simplekml.Color.green
+        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_square.png'     
+    if n >= 49 and n < 52:
+        # Other NSW Moorings
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
+        pnt.style.labelstyle.scale = 2 
+        pnt.style.iconstyle.color = simplekml.Color.green
+        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/ferry.png' 
+    if n >= 52 and n < 94:
+        # ATF moorings
+        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])        
+        pnt.style.iconstyle.color = simplekml.Color.green
+        pnt.style.labelstyle.scale = 1 
+        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/donut.png'
+          
 
   
 #kml.save('/Users/Michael/Documents/Projects/Python_Google_map/Newcastle_assets.kml')
@@ -44,7 +95,7 @@ for row in inputfile:
 # %% ------------------------------------------------
 # create Newcastle glider KML
 
-inputfile = csv.reader(open('/Users/Michael/Documents/Projects/Python_Google_map/glider_tracks_2020_0101-0107.csv','r'))
+inputfile = csv.reader(open('/Users/Michael/Documents/Projects/Python_Google_map/IMOS_assets_map_creator/glider_tracks_2020_0101-0107.csv','r'))
 
 #kml=simplekml.Kml()
 n = 0
@@ -52,13 +103,12 @@ for row in inputfile:
     n = n+1      
     if n % 1000 == 0:
         pnt = kml.newpoint(name='', coords=[(row[1],row[0])])    
-        pnt.style.labelstyle.color = simplekml.Color.coral
+        pnt.style.labelstyle.color = simplekml.Color.gray
         pnt.style.labelstyle.scale = 1 
         pnt.style.iconstyle.scale = 0.5
-        pnt.style.iconstyle.color = simplekml.Color.coral
+        pnt.style.iconstyle.color = simplekml.Color.grey
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/shaded_dot.png'    
     
-  
 #kml.save('/Users/Michael/Documents/Projects/Python_Google_map/Glider_tracks.kml')    
 
-kml.save('/Users/Michael/Documents/Projects/Python_Google_map/Newcastle_assets.kml')
+kml.save('/Users/Michael/Documents/Projects/Python_Google_map/IMOS_assets_map_creator/Newcastle_assets.kml')
