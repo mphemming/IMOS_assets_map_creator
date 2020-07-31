e88165b074fb03135010dbb645d3cdaf2ead0386
Michael Hemming <m.hemming@unsw.edu.au>
2020-07-31 12:12:11 +1000

    style changes, and now use legend strings for point names for easier legend creation in Google Earth

--- Newcastle_kml.py
+++ Newcastle_kml.py
@@ -20,17 +20,17 @@ for row in inputfile:
     n = n+1  
     if n< 6:
         # ATF moorings
-        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])        
+        pnt = kml.newpoint(name='Existing ATF moorings', coords=[(row[2],row[1])])        
         pnt.style.iconstyle.color = simplekml.Color.green
-        pnt.style.labelstyle.scale = 1 
-        pnt.iconstyle.scale = 0.5
+        pnt.style.labelstyle.scale = 0
+        pnt.iconstyle.scale = 1
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/donut.png'
     if n >= 6 and n < 8:
         # HF radar sites
-        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
+        pnt = kml.newpoint(name='Existing HF radar sites', coords=[(row[2],row[1])])              
         pnt.style.iconstyle.color = simplekml.Color.green
-        pnt.style.labelstyle.scale = 1 
-        pnt.iconstyle.scale = 0.5        
+        pnt.style.labelstyle.scale = 0 
+        pnt.iconstyle.scale = 1        
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'  
     if n >= 8 and n < 16:
         a = 1
@@ -41,16 +41,16 @@ for row in inputfile:
         #        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'  
     if n >= 16 and n < 19:
         # Proposed Newcastle moorings
-        pnt = kml.newpoint(name=row[0], coords=[(row[2],row[1])])              
-        pnt.style.labelstyle.scale = 1.5 
+        pnt = kml.newpoint(name='Proposed moorings', coords=[(row[2],row[1])])              
+        pnt.style.labelstyle.scale = 0
         pnt.style.labelstyle.color = simplekml.Color.white
         pnt.iconstyle.scale = 1       
         pnt.style.iconstyle.color = simplekml.Color.red
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_square.png'         
     if n >= 19 and n < 21: 
         # Proposed Sydney HF Radar sites
-        pnt = kml.newpoint(name=row[0], coords=[(row[2],row[1])])   
-        pnt.style.labelstyle.scale = 1.5 
+        pnt = kml.newpoint(name='Proposed HF radar sites', coords=[(row[2],row[1])])   
+        pnt.style.labelstyle.scale = 0
         pnt.style.labelstyle.color = simplekml.Color.white          
         pnt.style.iconstyle.color = simplekml.Color.red
         pnt.iconstyle.scale = 1        
@@ -64,10 +64,10 @@ for row in inputfile:
         #        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'  
     if n >= 30 and n < 32:        
         # Coffs Harbour radar sites
-        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
+        pnt = kml.newpoint(name='Existing HF radar sites', coords=[(row[2],row[1])])              
         pnt.style.iconstyle.color = simplekml.Color.green
-        pnt.style.labelstyle.scale = 1 
-        pnt.iconstyle.scale = 0.5        
+        pnt.style.labelstyle.scale = 0
+        pnt.iconstyle.scale = 1        
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'          
     if n >= 32 and n < 39:
         a = 1
@@ -78,24 +78,24 @@ for row in inputfile:
         #        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/target.png'          
     if n >= 40 and n < 49:
         # Other NSW Moorings
-        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
-        pnt.style.labelstyle.scale = 2 
-        pnt.iconstyle.scale = 0.5        
+        pnt = kml.newpoint(name='Existing ANMN moorings', coords=[(row[2],row[1])])              
+        pnt.style.labelstyle.scale = 0
+        pnt.iconstyle.scale = 1        
         pnt.style.iconstyle.color = simplekml.Color.green
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_square.png'     
     if n >= 49 and n < 52:
         # Other NSW Moorings
-        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])              
-        pnt.style.labelstyle.scale = 2 
-        pnt.iconstyle.scale = 0.5        
+        pnt = kml.newpoint(name='Existing biogeochemical sampling sites', coords=[(row[2],row[1])])              
+        pnt.style.labelstyle.scale = 0 
+        pnt.iconstyle.scale = 1        
         pnt.style.iconstyle.color = simplekml.Color.green
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png' 
     if n >= 52 and n < 94:
         # ATF moorings
-        pnt = kml.newpoint(name='', coords=[(row[2],row[1])])        
+        pnt = kml.newpoint(name='Existing ATF moorings', coords=[(row[2],row[1])])        
         pnt.style.iconstyle.color = simplekml.Color.green
-        pnt.iconstyle.scale = 0.5        
-        pnt.style.labelstyle.scale = 1 
+        pnt.iconstyle.scale = 1        
+        pnt.style.labelstyle.scale = 0 
         pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/donut.png'
           
 
